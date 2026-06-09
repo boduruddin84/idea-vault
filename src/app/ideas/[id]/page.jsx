338bf16af2f66d04cf2ec1485c95fba1e251ec60
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-06-09 18:39:46 +0600

    API updated

--- src/app/ideas/[id]/page.jsx
+++ src/app/ideas/[id]/page.jsx
@@ -1,5 +1,6 @@
 import IdeaDetails from "@/components/IdeaDetails";
 import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 
 const fetchSingleIdea = async (id, token) => {
@@ -23,11 +24,11 @@ const IdeaDetailsPage = async ({ params }) => {
     
 
   return (
+    
     <div className=" md:max-w-7xl mx-auto">
         <h1 className="my-10 ml-5 text-3xl font-bold text-center">Idea Details</h1>
         
         <IdeaDetails key={idea.id} idea={idea} />
-       
     </div>
   )
 }
