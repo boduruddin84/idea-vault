7314a1f41bc3ae70c42e63f5a2c2d0ec9e2c4b7e
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-06-16 12:29:46 +0600

    Comment form added

--- src/components/IdeaDetails.jsx
+++ src/components/IdeaDetails.jsx
@@ -15,10 +15,7 @@ const handleComment = async (e) => {
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URI}/comments`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
+    
     body: JSON.stringify(commentData),
   });
 
